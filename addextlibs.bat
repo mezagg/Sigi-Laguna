265ee6f16b1672d468748e15a85dbcca5598b1b3
Gerardo Meza <mezagg@gmail.com>
2016-02-11 08:39:43 -0600

    se agrega bat para incluir librerías externas

---
+++ addextlibs.bat
@@ -0,0 +1,5 @@
+mvn install:install-file -Dfile=./nsjp-etc/kaptcha-2.3.jar -DgroupId=com.google.code -DartifactId=kaptcha -Dversion=2.3 -Dpackaging=jar 
+
+mvn install:install-file -Dfile=./nsjp-etc/jdbc/sqljdbc4.jar -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=4.0 -Dpackaging=jar 
+
+
